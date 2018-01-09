2a9c769ef15a5d145be1013c3baf941bfd4c9b4c
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2018-01-09 01:46:09 +0100

    supressed instru test. added apk deploy

--- app/src/androidTest/java/ch/mse/biketracks/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/ch/mse/biketracks/MainActivityInstrumentedTest.java
@@ -32,6 +32,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
@@ -82,13 +83,33 @@ public class MainActivityInstrumentedTest {
             e.printStackTrace();
         }
 
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withId(R.id.appBarLayout3),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         ViewInteraction button = onView(
-                allOf(withId(R.id.start_recording),
+                allOf(withId(R.id.urgency),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.content_main),
+                                        IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class),
                                         0),
-                                2),
+                                0),
                         isDisplayed()));
         button.check(matches(isDisplayed()));
 
